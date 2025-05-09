5af49d9e537f9a7a9dd35c90aa4cdbce07368e35
Aala Azzam <78929333+Aala3zzaam@users.noreply.github.com>
2025-05-09 18:44:54 +0300

    updated app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model 
-
+from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.models import load_model
 
 model = load_model("tomato_leaf_diseases_detection.h5")
 
-
-classes = ['Bacterial spot', 'Early blight', 'Late blight', 'Leaf Mold', 
-           'Septoria leaf spot', 'Spider mites', 'Target Spot', 
+classes = ['Bacterial spot', 'Early blight', 'Late blight', 'Leaf Mold',
+           'Septoria leaf spot', 'Spider mites', 'Target Spot',
            'Yellow Leaf Curl Virus', 'Mosaic virus', 'Healthy']
-
 
 st.markdown("""
     <style>
@@ -38,13 +35,11 @@ st.header("🍅 Tomato Leaf Disease Detector!")
 st.subheader("Model Accuracy: 97.76%")
 st.markdown('</div>', unsafe_allow_html=True)
 
-
 image = st.file_uploader("Upload your picture here!")
 
 if image is not None:
     image = Image.open(image).convert("RGB")
     st.image(image, caption='The photo you uploaded', width=250)
-
     img = image.resize((224, 224))
     img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0
